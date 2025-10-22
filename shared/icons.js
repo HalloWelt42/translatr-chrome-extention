e2aacfae3b4fffa590b945483e760e4e054f4a4e
HalloWelt42 <my@jobmagnetix.de>
2025-10-22 15:48:43 +0200

    v1.10.1 - Fix: data-icon auf data-swt-icon umgestellt
    - Eigenes Attribut data-swt-icon statt generischem data-icon
    - Verhindert Konflikte mit Website-Icons (discord, bluesky, github etc.)
    - Content-Script greift nicht mehr in fremde data-icon Attribute ein
    - Alle HTML, CSS und JS Referenzen angepasst

--- shared/icons.js
+++ shared/icons.js
@@ -3,7 +3,7 @@
  * FontAwesome 6.x Solid (OFL/MIT lizenziert)
  *
  * Verwendung:
- * - HTML: <span data-icon="language"></span> + SWT.Icons.init()
+ * - HTML: <span data-swt-icon="language"></span> + SWT.Icons.init()
  * - JS:   SWT.Icons.svg('language') -> HTML-String
  * - JS:   SWT.Icons.create('language') -> SVGElement
  */
@@ -63,7 +63,7 @@ SWT.Icons = {
     'scale-balanced': { vb: '0 0 640 512', d: 'M384 32l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L398.4 96c-5.2 25.8-22.9 47.1-46.4 57.3L352 448l160 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-192 0-192 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l160 0 0-294.7c-23.5-10.3-41.2-31.6-46.4-57.3L128 96c-17.7 0-32-14.3-32-32s14.3-32 32-32l128 0c14.6-19.4 37.8-32 64-32s49.4 12.6 64 32zm55.6 288l144.9 0L512 195.8 439.6 320zM512 416c-62.9 0-115.2-34-126-78.9c-2.6-11 1-22.3 6.7-32.1l95.2-163.2c5-8.6 14.2-13.8 24.1-13.8s19.1 5.3 24.1 13.8l95.2 163.2c5.7 9.8 9.3 21.1 6.7 32.1C627.2 382 574.9 416 512 416zM126.8 195.8L54.4 320l144.9 0L126.8 195.8zM.9 337.1c-2.6-11 1-22.3 6.7-32.1l95.2-163.2c5-8.6 14.2-13.8 24.1-13.8s19.1 5.3 24.1 13.8l95.2 163.2c5.7 9.8 9.3 21.1 6.7 32.1C242 382 189.7 416 126.8 416S11.7 382 .9 337.1z' },
     'pen-to-square': { vb: '0 0 512 512', d: 'M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-96c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 64z' },
 
-    // Aliase (Kompatibilitaet mit alten data-icon Attributen)
+    // Aliase (Kompatibilitaet mit alten data-swt-icon Attributen)
     translate: null, // -> language
     'delete': null,  // -> trash
     refresh: null,   // -> arrow-rotate-right
@@ -125,8 +125,8 @@ SWT.Icons = {
 
   init() {
     this._setupAliases();
-    document.querySelectorAll('[data-icon]').forEach(function(el) {
-      var name = el.dataset.icon;
+    document.querySelectorAll('[data-swt-icon]').forEach(function(el) {
+      var name = el.dataset.swtIcon;
       var svg = SWT.Icons.svg(name);
       if (svg && !el.querySelector('svg')) {
         el.innerHTML = svg;
